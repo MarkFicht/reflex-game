b985daa725736ba2cc7d3d4022a431ae22e5cc02
Marek <marekficht@gmail.com>
2019-02-21 22:48:06 +0100

    Refactoring GameDisconnect.js - main variable from Firebase/game changed here

--- src/containers/GameDisconnect.js
+++ src/containers/GameDisconnect.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import * as firebase from 'firebase';
 
 import logo from '../components/other/logo';
 
 
 class GameDisconnect extends Component {
+    _isMounted = false;
+
     constructor(props) {
         super(props);
         this.state = {
@@ -14,26 +17,40 @@ class GameDisconnect extends Component {
     };
 
     componentDidMount() {
-        this.redirectInterval = setInterval( () => {
-            this.setState({
-                redirectTime: this.state.redirectTime - 1
+        this._isMounted = true;
+
+        if (this._isMounted) {
+
+            this.redirectInterval = setInterval(() => {
+
+                this.setState({
+                    redirectTime: this.state.redirectTime - 1
+                })
+            }, 1000)
+
+            /** Restart: Bool for checking connected 2 players */
+            firebase.database().ref('/game').update({
+                disconnect: false,
             })
-        }, 1000 )
+        }
+        
     }
 
     componentWillUnmount() {
+        this._isMounted = false;
         clearTimeout(this.redirectInterval);
     }
 
-    //--- RENDER ---//
     render() {
+        const { redirectTime } = this.state;
+        
         return (
             <div className='disconnected'>
                 { logo }
                 <p> Jeden z graczy wyszedł lub został rozłączony. </p>
                 <p> Gra została przerwana. </p>
-                <h3>{ `Przekierowanie za: ${this.state.redirectTime}` }</h3>
-                { this.state.redirectTime < 0 && <Redirect to='/' />}
+                <h3>{ `Przekierowanie za: ${ redirectTime }` }</h3>
+                { redirectTime < 0 && <Redirect to='/' />}
             </div>
         );
     }
